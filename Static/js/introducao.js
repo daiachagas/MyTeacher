69b69d44c434207e689bbdd83764e35635c18209
daiachagas <daiachagas188@gmail.com>
2022-11-28 22:46:40 -0300

    first commit

---
+++ Static/js/introducao.js
@@ -0,0 +1,36 @@
+// Comando que exibe informações no console
+// console.log("Olá Mundo!")
+
+//Variaveis e tipos de dados
+//string
+let nome = "Daia Chagas"
+console.log(nome)
+
+//number
+let idade = 21
+console.log(idade)
+
+//boolean
+let ehProgramador = true
+console.log(ehProgramador)
+
+//Operações matemativas
+let num1 = 10
+let num2 = 5
+
+//soma
+console.log("soma")
+console.log(num1 + num2)
+
+
+//subtração
+console.log("subtração")
+console.log(num1 - num2)
+
+//multiplicação
+console.log("multiplicação")
+console.log(num1 - num2)
+
+//divisão
+console.log("divisão")
+console.log
